1d66310e607fbe242f8c9ea3a3ca5ed820c5cb58
MoraRamirez <207440367@ina.cr>
2021-08-27 09:47:47 -0600

    Nueva actualizacion

---
+++ JS/funcionalidad.js
@@ -0,0 +1,19 @@
+let num1 = 0;
+let num2 = 0;
+
+
+function sumar(num){
+  num1 = document.getElementById('num1Sum').value;
+  num2 = docuemq.getElementById('num2Sum').value;
+
+  introducirNumero(num,num1);
+  introducirNumero(num,num2);
+}
+
+function introducirNumero(nummber,contenedor){
+    if (contenedor='') {
+        contenedor = number;
+    }else{
+        contenedor = contenedor += number
+    }
+}
